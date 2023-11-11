03bf8601f923a2f31d7697dad438f5ab9d156183
Kota Yuhara <hotwater1367@gmail.com>
2023-11-11 18:46:58 +0900

    update

--- build.py
+++ build.py
@@ -13,7 +13,11 @@ def build_script():
     to_encode = list(Path('kaggle_petfinder').glob('*.py')) + [Path('setup.py')]
     file_data = {str(path.as_posix()): encode_file(path) for path in to_encode}
     template = Path('script_template.py').read_text('utf8')
-    Path('build/script.py').write_text(
+    build_dir = Path("build")
+    if not build_dir.exists():
+        build_dir.mkdir(parents=True, exist_ok=True)
+    script_path = build_dir / "script.py"
+    script_path.write_text(
         template.replace('{file_data}', str(file_data)),
         encoding='utf8')
 
